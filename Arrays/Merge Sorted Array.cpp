2b8e0889d3fdd1fb189e8b10213e903da6a8fc04
Madhok <gsmadhok@gmail.com>
2015-06-20 18:11:53 -0700

    Update Merge Sorted Array.cpp

--- Arrays/Merge Sorted Array.cpp
+++ Arrays/Merge Sorted Array.cpp
@@ -5,7 +5,37 @@ Note:
 You may assume that A has enough space to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
 
 */
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(n==0) return;
+        if(m==0) {
+            for(int i = 0; i < n ; i++) {
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        
+        int k = m + n -1;
+        m--;
+        n--;
+        while(m >= 0 && n >=0) {
 
+            if(nums1[m] >= nums2[n]) {
+                nums1[k--] = nums1[m--];
+            } else {
+                nums1[k--] = nums2[n--];
+            }
+        }
+        
+        while(n>=0) {
+            nums1[k--] = nums2[n--];
+        }
+        return;
+    }
+};
+
+// For loop is confusing
 class Solution {
 public:
     void merge(int A[], int m, int B[], int n) {
