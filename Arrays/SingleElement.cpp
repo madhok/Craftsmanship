8fe0964870d2951dae6c3d8da4ed716f6e202c13
Madhok <gsmadhok@gmail.com>
2015-07-05 10:00:52 -0700

    Update SingleElement.cpp

--- Arrays/SingleElement.cpp
+++ Arrays/SingleElement.cpp
@@ -5,7 +5,25 @@ Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
 */
-
+ int singleNumber(vector<int>& nums) {
+        int size = nums.size();
+        if(size == 0) return -1;
+        if(size  < 2) return nums[0];
+        
+        int result = 0;
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            int x = 1 << i;
+            for(int j = 0; j <size; j++) {
+                if(nums[j] & x)
+                    sum++;
+            }
+            if(sum%3)
+                result |= x;
+        }
+        return result;
+    }
+    
 int getSingle(int arr[], int n)
 {
 	// Initialize result
