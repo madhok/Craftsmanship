e68f80632b0ac498aa445b3958a3a0eb37b02dd8
Madhok <gsmadhok@gmail.com>
2015-07-05 09:58:58 -0700

    Create SingleElement.cpp

---
+++ Arrays/SingleElement.cpp
@@ -0,0 +1,36 @@
+/*
+Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+*/
+
+int getSingle(int arr[], int n)
+{
+	// Initialize result
+	int result = 0;
+
+	int x, sum;
+    
+	// Iterate through every bit
+	for (int i = 0; i < INT_SIZE; i++)
+	{
+	// Find sum of set bits at ith position in all
+	// array elements
+	sum = 0;
+	x = (1 << i);
+	for (int j=0; j< n; j++ )
+	{
+		if (arr[j] & x)
+			sum++;
+	}
+
+	// The bits with sum not multiple of 3, are the
+	// bits of element with single occurrence.
+	if (sum % 3)
+		result |= x;
+	}
+
+	return result;
+}
