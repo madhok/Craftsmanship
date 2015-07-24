4c198285444a2c7210ddb947b21181394c453d1f
Madhok <gsmadhok@gmail.com>
2015-07-24 14:25:02 -0700

    Update PathSumBT.cpp

--- BinaryTree/PathSumBT.cpp
+++ BinaryTree/PathSumBT.cpp
@@ -1,4 +1,33 @@
 /*
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root == NULL ) {
+           return false;
+        }
+        
+        if(root->left == NULL && root->right == NULL) {
+            if(root->val == sum)
+                return true;
+        } 
+        return hasPathSum(root->left, sum-root->val) || 
+                hasPathSum(root->right, sum- root->val);
+        
+    }
+};
+
+
+*/
+/*
 Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 
 For example:
@@ -54,3 +83,4 @@ public:
         return (getSumtoleaf(Node->left, sum - Node->val) || getSumtoleaf(Node->right,  sum - Node->val));
     }
 };
+
